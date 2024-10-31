8bab125551ec5055dba912d41ef841563212c478
Nikita Luzianin <s2400796@edu.bc.fi>
2024-10-31 14:51:39 +0200

    update bak to top button

--- portfolio/main.js
+++ portfolio/main.js
@@ -132,21 +132,23 @@ function scrollFunction() {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
         //document.querySelector("header").style.backdropFilter = "blur(100px)";
         document.querySelector("header").style.backgroundColor = "rgb(0 0 0 / 80%)";
-        if (scrollPosition[0] - document.body.scrollTop > minStepToBecomeScrolling || scrollPosition[1] - document.documentElement.scrollTop > minStepToBecomeScrolling) {
-            backToTopButton.style.display = "block";
-            document.querySelector("header").style.display = "flex";
-        } else {
-            backToTopButton.style.display = "none";
-            document.querySelector("header").style.display = "none";
-            //document.querySelector("header").style.height = "100px";
-            //document.querySelector("header, logo h1").style.fontSize = "12px";
-        }
-
-        // if scrolling top then show the controlling elements
     } else {
         document.querySelector("header").style.backgroundColor = "Transparent";
 
     }
+
+    if (scrollPosition[0] - document.body.scrollTop > minStepToBecomeScrolling || scrollPosition[1] - document.documentElement.scrollTop > minStepToBecomeScrolling) {
+        backToTopButton.style.display = "block";
+        document.querySelector("header").style.display = "flex";
+    } else {
+        backToTopButton.style.display = "none";
+        document.querySelector("header").style.display = "none";
+        //document.querySelector("header").style.height = "100px";
+        //document.querySelector("header, logo h1").style.fontSize = "12px";
+    }
+
+    // if scrolling top then show the controlling elements
+
     scrollPosition[0] = document.body.scrollTop;
     scrollPosition[1] = document.documentElement.scrollTop;
 
